1218ee3696a0c3a07ee96b81719960db15c890d9
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2021-06-01 21:37:42 -0300

    Fisrt commit

---
+++ src/test/java/com/avaliacao/avaliacao/AvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
